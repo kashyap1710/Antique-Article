20cf16e2e4a7fb3dd4f827e766607d9307a00fb9
kashyap1710 <kashyapbhesdadia1710@gmail.com>
2024-12-26 19:30:02 +0530

    Used forward refrence in input text

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,12 @@
 import Header from './Header';
 import Footer from './Footer';
+import Container from './container/Container';
+import Logo from './Logo';
+import LogOutBtn from './Header/LogOutBtn';
 
-export { Header, Footer };
+export { 
+    Header,
+    Footer,
+    Container,
+    Logo
+};
